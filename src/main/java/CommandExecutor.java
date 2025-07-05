7e593b1242b54761593b85a8af5b4e9e80b295e4
Gowshik <gowshikprabhu@gmail.com>
2025-07-06 00:43:49 +0530

    codecrafters submit [skip ci]

--- src/main/java/CommandExecutor.java
+++ src/main/java/CommandExecutor.java
@@ -7,14 +7,25 @@ public class CommandExecutor {
             return getPongResponse();
         } else if ("command".equalsIgnoreCase(command)) {
             return getAvailableCommands();
+        } else if ("echo".equalsIgnoreCase(command)) {
+            return getEchoResponse(args);
         } else {
             LoggingService.logError("Unknown command: " + command + " with args: " + args);
             return "-ERR unknown command '" + command + "'\r\n";
         }
     }
 
+    private String getEchoResponse(List<String> args) {
+        if (args.isEmpty()) {
+            return RESPEncoder.encodeError("ERR echo requires at least one argument");
+        }
+        String arg = args.getFirst();
+        LoggingService.logFine("Echoing: " + arg);
+        return RESPEncoder.encodeBulkString(arg);
+    }
+
     private String getAvailableCommands() {
-        List<String> commands = List.of("ping", "command");
+        List<String> commands = List.of("ping", "command", "echo");
         LoggingService.logFine("Sending command list COMMAND.");
         return RESPEncoder.encodeStringArray(commands);
     }
